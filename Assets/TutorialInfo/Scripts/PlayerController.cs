c6a9addd963d748580955358512b4fcee12365b0
Sebarbosa10 <sebastianbarbosa10@gmail.com>
2025-04-21 20:41:07 -0300

    Movimiento del player con FMS y agregando crouch

--- Assets/TutorialInfo/Scripts/PlayerController.cs
+++ Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -2,26 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
-    IMove _move;
-    ILook _look;
+    FSM<StateEnum> _fsm;
 
     private void Awake()
     {
-        _move = GetComponent<IMove>();
-        _look = GetComponent<ILook>();
+        InitializeFSM();
     }
+
+    void InitializeFSM()
+    {
+        _fsm = new FSM<StateEnum>();
+        var move = GetComponent<IMove>();
+        var look = GetComponent<ILook>();
+        var attack = GetComponent<IAttack>();
+        var crouchHandler = GetComponent<ICrouch>(); // <- ESTA es la interfaz
+
+        var stateList = new List<PSBase<StateEnum>>();
+
+        var idle = new PSIdle<StateEnum>(StateEnum.Walk);
+        var walk = new PSWalk<StateEnum>(StateEnum.Idle);
+        var crouch = new PSCrouch<StateEnum>(StateEnum.Idle); // <- ESTE es el estado
+
+        // Transiciones
+        idle.AddTransition(StateEnum.Walk, walk);
+        idle.AddTransition(StateEnum.Crouch, crouch);
+
+        walk.AddTransition(StateEnum.Idle, idle);
+        walk.AddTransition(StateEnum.Crouch, crouch);
+
+        crouch.AddTransition(StateEnum.Idle, idle);
+
+        // Agregar estados a la lista
+        stateList.Add(idle);
+        stateList.Add(walk);
+        stateList.Add(crouch);
+
+        // Inicializar estados
+        foreach (var state in stateList)
+        {
+            state.Initialize(move, look, attack, crouchHandler); // ¡acá se pasa la interfaz!
+        }
+
+        // Estado inicial
+        _fsm.SetInit(idle);
+    }
+
     private void Update()
     {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
-
-        var dir = new Vector3(h, 0, v);
-        _move.Move(dir);
-        if (h != 0 || v != 0)
+        if (Input.GetKey(KeyCode.LeftControl))
         {
-            _look.LookDir(dir);
+            _fsm.Transition(StateEnum.Crouch);
         }
+
+        _fsm.OnExecute();
     }
 }
---
+++ Assets/TutorialInfo/Scripts/Playerstate/PSWalk.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSWalk<T> : PSBase<T>
+{
+    T _inputToWalk;
+    public PSWalk(T inputToIdle)
+    {
+        _inputToWalk = inputToIdle;
+    }
+    public override void Enter()
+    {
+        base.Enter();
+        Debug.Log("WALK: ENTER");
+    }
+    public override void Execute()
+    {
+        var dir = new Vector3(InputManager.GetMove().x, 0, InputManager.GetMove().y);
+        _move.Move(dir);
+        if (dir != Vector3.zero)
+        {
+            _look.LookDir(dir);
+        }
+        else
+        {
+            StateMachine.Transition(_inputToWalk);
+        }
+    }
+}
