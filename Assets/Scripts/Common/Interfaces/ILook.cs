13e7d77ff8d7832c1033e787fa99391b0e2532b6
SatoruuGoyoo <mentabullrichgregorio@gmail.com>
2025-04-22 19:27:21 -0300

    Merge con Player

--- Assets/Scripts/Common/Interfaces/ILook.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface ILook 
-{
-    void LookDirection(Vector3 dir);
-    
-}
--- Assets/Scripts/Common/Interfaces/IMove.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IMove 
-{
-    void Move(Vector3 dir);
-}
