86e995b7ce25288522eb759743719962a595190e
MaxiB <125215026+Maximo9002@users.noreply.github.com>
2025-04-24 20:14:11 -0300

    creando script del random

--- Assets/Scripts/Randomizer/SoundRandom.cs
+++ Assets/Scripts/Randomizer/SoundRandom.cs
@@ -6,14 +6,29 @@ public class SoundRandom : MonoBehaviour
 {
     public SoundDataBase dataBase;
     public AudioSource audioSource;
-    void Start()
+    Dictionary<RarityEnum, float> _weights;
+    public float interval = 5f;
+
+    private void Awake()
     {
-        
+        _weights = new Dictionary<RarityEnum, float>();
+        {
+            { RarityEnum.Common, 1f},
+            { RarityEnum.Rare, 0.5f},
+            { RarityEnum.UltraRare, 0.1f}
+        };
+
+        StartCoroutine(PlayRandomSoundLoop());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PlayRandomSoundLoop()
     {
-        
+        while (true)
+        {
+            GetRandomSound();
+            yield return new WaitForSeconds(interval);
+        } 
     }
+
+    void GetRandomSound()
 }
