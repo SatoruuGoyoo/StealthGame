3e20addc22ea3649bdee4b5b826a5876f0ed0951
Nicolas Saldeno <skyladdamebusiness@gmail.com>
2025-04-26 22:01:09 -0300

    adding alarm and detect

--- Assets/Scripts/Miscellanous/GameStates/GameOverState.cs
+++ Assets/Scripts/Miscellanous/GameStates/GameOverState.cs
@@ -5,6 +5,7 @@ public class GameOverState : State<GameState>
     public override void Enter()
     {
         Debug.Log("Game Over");
+        Time.timeScale = 0f;
     }
 
     public override void Exit()
---
+++ Assets/Scripts/Miscellanous/VictoryAndLose/AlarmTrigger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AlarmTrigger : MonoBehaviour
+{
+    public GameObject[] alarmLights; 
+    public DefeatHandler defeatHandler;
+    private bool alarmActivated = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!alarmActivated && other.CompareTag("NPC2"))
+        {
+            alarmActivated = true;
+
+            if (defeatHandler != null)
+                defeatHandler.Defeated = true;
+
+            foreach (var lightObj in alarmLights)
+            {
+                if (lightObj != null)
+                    lightObj.SetActive(true);
+            }
+        }
+        Time.timeScale = 0f;
+    }
+}
