103f4974aec17ca0ac844f9970c3be0f02b54724
MaxiB <125215026+Maximo9002@users.noreply.github.com>
2025-04-27 16:10:57 -0300

    haciendo que el enemy reproduzca un sonido cuando ve al player

--- Assets/Scripts/NPC/NPCView.cs
+++ Assets/Scripts/NPC/NPCView.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class NPCView : PlayerView
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private LineOfSightMono _los;
+    [SerializeField] private Transform _player;
+    [SerializeField] private AudioSource _audioSource;
+    private bool _hasSeenPlayer = false;
+
+    private void Start()
     {
-        
+        if (_los == null)
+        {
+            _los = GetComponent<LineOfSightMono>();
+        }
+        if (_audioSource == null)
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+        if (_player == null && GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            _player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private new void Update()
     {
-        
+        base.Update();
+
+        if (_player == null) return;
+
+        bool seesPlayer = _los.LOS(_player);
+
+        if (seesPlayer && !_hasSeenPlayer)
+        {
+            _hasSeenPlayer = true;
+            PlaySpottedSound();
+        }
+        else if (!seesPlayer)
+        {
+            _hasSeenPlayer = false;
+        }
+    }
+
+    private void PlaySpottedSound()
+    {
+        if (_audioSource != null && _audioSource.isPlaying)
+        {
+            _audioSource.Play();
+        }
     }
 }
